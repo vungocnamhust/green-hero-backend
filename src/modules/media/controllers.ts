d2dc8110daa193dce3d20868afaa12a1086ad13d
Nam Vu <vungocnam0409@gmail.com>
2021-08-28 20:55:49 +0700

    fix: bug crash server by media

--- src/modules/media/controllers.ts
+++ src/modules/media/controllers.ts
@@ -1,20 +1,26 @@
 import fs from 'fs';
+import codes from '../../errors/codes';
+import CustomError from '../../errors/customError';
 
 const getMedia = (req, res) => {
-    const fileType = req.params.type;
-    const filePath = "./uploads/" + req.params.filePath;
-    const audioStream = fs.createReadStream(filePath);
-    const fileStat = fs.statSync(filePath);
-    if (fileType == "image") {
-        res.setHeader('Content-Type', 'image/jpeg');
-        res.setHeader('Content-Length', fileStat.size);
-        res.setHeader('Accept-Ranges', 'bytes');
-    } else if (fileType == "video") {
-        res.setHeader('Content-Type', 'video/mp4');
-        res.setHeader('Content-Length', fileStat.size);
-        res.setHeader('Accept-Ranges', 'bytes');
+    try {
+        const fileType = req.params.type;
+        const filePath = "./uploads/" + req.params.filePath;
+        const audioStream = fs.createReadStream(filePath);
+        const fileStat = fs.statSync(filePath);
+        if (fileType == "image") {
+            res.setHeader('Content-Type', 'image/jpeg');
+            res.setHeader('Content-Length', fileStat.size);
+            res.setHeader('Accept-Ranges', 'bytes');
+        } else if (fileType == "video") {
+            res.setHeader('Content-Type', 'video/mp4');
+            res.setHeader('Content-Length', fileStat.size);
+            res.setHeader('Accept-Ranges', 'bytes');
+        }
+        audioStream.pipe(res);
+    } catch (e) {
+        throw new CustomError(codes.BAD_REQUEST);
     }
-    audioStream.pipe(res);
 };
 
 export default {
